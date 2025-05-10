7e8402bd0c4ed1c265da4f8743742251ca5a2211
witekmikolajczak <witekmikolajczak@gmail.com>
2025-05-10 19:28:39 +0200

    chore: material ui  + redux

---
+++ app/store/store.ts
@@ -0,0 +1,21 @@
+import {
+  configureStore,
+  type ThunkAction,
+  type Action,
+} from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
